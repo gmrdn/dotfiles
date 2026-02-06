02a9fd3c74d89d8864afb544bca4abe11c4a4afd
Guillaume Rondon <guillaume.rondon@gmail.com>
2026-02-06 10:59:06 +0100

    feat(nvim): fix picker display to use in multi packages

---
+++ nvim/lua/plugins/picker.lua
@@ -0,0 +1,22 @@
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    picker = {
+      formatters = {
+        file = {
+          filename_first = true, -- display filename before the file path
+          --- * left: truncate the beginning of the path
+          --- * center: truncate the middle of the path
+          --- * right: truncate the end of the path
+          ---@type "left"|"center"|"right"
+          truncate = "right",
+          min_width = 40, -- minimum length of the truncated path
+          filename_only = false, -- only show the filename
+          icon_width = 2, -- width of the icon (in characters)
+          git_status_hl = true, -- use the git status highlight group for the filename
+        },
+      },
+    },
+  },
+}
