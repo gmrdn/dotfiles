13d65059eb51d8a7d37284cd08f39823de388ff7
Guillaume Rondon <guillaume.rondon@gmail.com>
2026-02-27 11:26:37 +0100

    plugins import

--- nvim/init.lua
+++ nvim/init.lua
@@ -154,3 +154,42 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt_local.spell = true
 	end,
 })
+
+-- =====================
+-- plugins (vi.pack)
+-- =====================
+vim.pack.add({
+	"https://www.github.com/lewis6991/gitsigns.nvim",
+	"https://www.github.com/echasnovski/mini.nvim",
+	"https://www.github.com/ibhagwan/fzf-lua",
+	"https://www.github.com/nvim-tree/nvim-tree.lua",
+	{
+		src = "https://github.com/nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		build = ":TSUpdate",
+	},
+	-- Language Server Protocols
+	"https://www.github.com/neovim/nvim-lspconfig",
+	"https://github.com/mason-org/mason.nvim",
+	"https://github.com/creativenull/efmls-configs-nvim",
+	{
+		src = "https://github.com/saghen/blink.cmp",
+		version = vim.version.range("1.*"),
+	},
+	"https://github.com/L3MON4D3/LuaSnip",
+})
+
+local function packadd(name)
+	vim.cmd("packadd " .. name)
+end
+packadd("nvim-treesitter")
+packadd("gitsigns.nvim")
+packadd("mini.nvim")
+packadd("fzf-lua")
+packadd("nvim-tree.lua")
+-- LSP
+packadd("nvim-lspconfig")
+packadd("mason.nvim")
+packadd("efmls-configs-nvim")
+packadd("blink.cmp")
+packadd("LuaSnip")
